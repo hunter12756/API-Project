0d1c266bd978d19b29a94e70b5964fd48961ccbc
hunter12756 <hlwatson17256@gmail.com>
2023-04-12 16:28:23 -0400

    Initialize Express and Sequelize with CSRF protection

---
+++ backend/app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+
+//routes
+const routes = require('./routes');
+
+//environment stuff
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
+//init express
+const app = express();
+
+
+//morgan middleware
+app.use(morgan('dev'));
+
+//parsers
+app.use(cookieParser());
+app.use(express.json());
+
+//security stuff
+// Security Middleware
+if (!isProduction) {
+    // enable cors only in development
+    app.use(cors());
+  }
+
+  // helmet helps set a variety of headers to better secure your app
+  app.use(
+    helmet.crossOriginResourcePolicy({
+      policy: "cross-origin"
+    })
+  );
+
+  // Set the _csrf token and create req.csrfToken method
+  app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+  );
+
+  //routes
+app.use(routes);
+  module.exports = app;
