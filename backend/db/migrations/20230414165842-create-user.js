837802b6830c585ba74f063eea756ded4d07efb9
hunter12756 <hlwatson17256@gmail.com>
2023-04-17 04:40:29 -0400

    maye

--- backend/db/migrations/20230414165842-create-user.js
+++ backend/db/migrations/20230414165842-create-user.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let options = {};
-if (process.env.NODE_ENV === 'practice_db') {
+if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
 
