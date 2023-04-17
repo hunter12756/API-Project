12bd541dd7191c584884cdcfc673660ba3108fb6
hunter12756 <hlwatson17256@gmail.com>
2023-04-17 05:06:34 -0400

    pls i swithc

--- backend/db/seeders/20230414170136-demo-user.js
+++ backend/db/seeders/20230414170136-demo-user.js
@@ -2,7 +2,7 @@
 const bcrypt = require("bcryptjs");
 
 let options = {};
-if (process.env.NODE_ENV === 'practice_db') {
+if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
 
