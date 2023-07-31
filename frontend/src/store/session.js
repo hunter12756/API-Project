154c846f966a46c7f5888b418af22dc8d369cf36
hunter12756 <hlwatson17256@gmail.com>
2023-07-31 12:53:18 -0400

    auth me phase 1

---
+++ frontend/src/store/session.js
@@ -0,0 +1,54 @@
+import { csrfFetch } from "./csrf";
+
+const SET_USER = "session/setUser"
+const REMOVE_USER = "session/removeUser"
+
+const setUser= (user)=> {
+    return {
+        type: SET_USER,
+        payload: user,
+    }
+}
+
+const removeUser= ()=> {
+    return {
+        type: SET_USER,
+    }
+}
+export const login = (user) => async (dispatch) => {
+    const {credential, password} = user;
+    try {
+        const res = await csrfFetch("/api/session", {
+            method:"POST",
+            body: JSON.stringify({
+                credential,
+                password,
+            }),
+        });
+        const data = await res.json();
+        if(res.ok){
+            dispatch(setUser(data.user))
+        }
+    } catch (error) {
+        const data = await error.json();
+        throw data;
+    }
+}
+const initialState = {user:null}
+
+const sessionReducer = (state = initialState, action)=> {
+    let newState;
+    switch(action.type) {
+        case SET_USER:
+            newState = Object.assign({},state);
+            newState.user = action.payload;
+            return newState;
+        case REMOVE_USER:
+            newState= Object.assign({},state);
+            newState.user = null;
+            return newState;
+        default:
+            return state;
+    }
+}
+export default sessionReducer;
